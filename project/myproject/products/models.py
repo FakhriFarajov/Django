211cd064ca730b1db6ec7b51ae17ca44e454a067
Farad_ru99 <fakhri_farajov@mail.cu>
2026-04-23 21:52:20 +0400

    new

---
+++ project/myproject/products/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, related_name='reviews', on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User', related_name='reviews', on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Review for {self.product.name} - Rating: {self.rating}'
---
+++ project/myproject/products/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import get_object_or_404, redirect, render
+from models import Product, Review
+
+# Create your views here.
+def product_list_view(request):
+    products = Product.objects.all()
+    return render(request, "products/product_list.html", {
+        "products": products,
+    })
+
+def product_detail_view(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    reviews = product.reviews.all()
+    return render(request, "products/product_detail.html", {
+        "product": product,
+        "reviews": reviews,
+    })
+
+
